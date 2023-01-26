95d3f963f4ee5a3c124e408c032bc6f2f407274a
eef-g <etan328@gmail.com>
2023-01-26 09:38:59 -0800

    Conflict Resolution Test pt.3

---
+++ Makefile
@@ -0,0 +1,5 @@
+all: main.c
+	gcc -Wall -Wextra -pedantic -std=c99 -o main.exe main.c list_files/node.c list_files/list.c list_files/stack.c
+.PHONY: clean
+clean:
+	rm main.exe
---
+++ main.c
@@ -0,0 +1,13 @@
+#include "list_files/stack.h"
+#include <stdio.h>
+#include<stdlib.h>
+
+
+int main()
+{
+
+    printf("=-=-=-=-=-=-=- Stack Testing =-=-=-=-=-=-=-\n");
+    Stackptr test_stack = Stack_new();
+        
+    printf("=-=-=-=-=-=-=- End Stack Testing =-=-=-=-=-=-=-\n");
+}
